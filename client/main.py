225ed68623f436e3e72ce442868dbcd76b0251b7
Badr <badrkmc@gmail.com>
2023-05-19 19:11:01 +0500

    allow users to reappeal if duration done

--- client/main.py
+++ client/main.py
@@ -146,6 +146,16 @@ async def profile():
     if user_ban_appeal_data:
         if user_ban_appeal_data.get("reappeal_time") and user_ban_appeal_data["reappeal_time"] > 0:
             user_ban_appeal_data["reappeal_time"] = round((user_ban_appeal_data["reappeal_time"] - time.time()) / (30 * 24 * 60 * 60), 2)
+    
+    # If reappeal time is / has reached 0 and ban is not permanent, then user can reappeal
+    if user_ban_appeal_data and user_ban_appeal_data.get("reappeal_time", 1) <= 0:
+        if not user_ban_appeal_data["permanent"]:
+            database.bans.find_one_and_update(
+                { "user_id": user_id },
+                { "$set": { "current_appeal": None } }
+            )
+
+            user_ban_appeal_data = None
 
     return await render_template("profile.html", user_data=user_data, user_ban_appeal_data=user_ban_appeal_data, ban_entry=ban_entry)
 
